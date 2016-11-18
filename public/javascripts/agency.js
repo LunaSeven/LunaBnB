51311c6dce0cecc42a5d6b226cd6a417a3d75dd8
lunaseven <june9309@gmail.com>
2016-11-19 03:10:44 +0900

    expressjs first commit

---
+++ public/javascripts/agency.js
@@ -0,0 +1,9 @@
+// navbar change class
+$(window).scroll(function() {
+    if ($("#mainNav").offset().top > 500) {
+        $("#mainNav").addClass("navbar-inverse");
+    } else {
+      $("#mainNav").removeClass("navbar-inverse");
+
+    }
+});
