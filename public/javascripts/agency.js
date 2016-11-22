8b010e2640b0fcf248f431b965f0d258755275b0
lunaseven <june9309@gmail.com>
2016-11-23 03:09:03 +0900

    dynamic sidebar

--- public/javascripts/agency.js
+++ public/javascripts/agency.js
@@ -1,8 +1,24 @@
 // navbar change class
 $(window).scroll(function() {
+
     if ($("#mainNav").offset().top > 500) {
         $("#mainNav").addClass("navbar-inverse");
+
+        if($(window).width()>1200){
+          var height= $("header").css("height");
+          height=parseInt(height)/1.5;
+          $("#sidebar").css("position","fixed");
+          $("#sidebar").css("margin-top","-"+height+"px");
+          $("#sidebar").css("margin-right","190px");
+        }
     } else {
       $("#mainNav").removeClass("navbar-inverse");
+      $("#sidebar").css("position","inherit");
+      $("#sidebar").css("margin-top","10px");
+      $("#sidebar").css("margin-right","0px");
+
+      //
+      //
+      //
     }
 });
