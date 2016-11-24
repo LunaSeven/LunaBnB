71ddcec50b6e4c065cc603c1d04358757db1d30d
lunaseven <june9309@gmail.com>
2016-11-25 05:13:42 +0900

    favorite button

---
+++ public/javascripts/rooms/room.js
@@ -0,0 +1,22 @@
+$('.btn_favorite').click(function(){
+  if($(this).hasClass('selected')){
+
+    $(this).removeClass('btn-danger');
+    $(this).removeClass('fa-heart');
+    $(this).addClass('btn-info');
+    $(this).addClass('fa-heart-o');
+    $(this).removeClass('selected');
+
+
+  }else{
+    $(this).removeClass('btn-info');
+    $(this).removeClass('fa-heart-o');
+
+    $(this).addClass('btn-danger');
+    $(this).addClass('fa-heart');
+    $(this).addClass('selected');
+
+  }
+
+
+});
