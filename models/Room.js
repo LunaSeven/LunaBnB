7e823ae9208587a43681658156da0b14af9c66fe
lunaseven <june9309@gmail.com>
2016-11-30 02:01:18 +0900

    register room first test

---
+++ models/Room.js
@@ -0,0 +1,27 @@
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema;
+
+var schema = new Schema({
+  host_email: {type: String},
+  room_name: {type: String},
+  room_intro: {type: String},
+  city: {type: String},
+  post_code: {type: String},
+  addr1: {type: String},
+  addr2: {type: String},
+  latitude: {type: String},
+  longitude: {type: String},
+  fee: {type: Number},
+  facillities: {type: String},
+  rules: {type: String},
+  image: {type: Object},
+  createdAt: {type: Date, default: Date.now}
+}, {
+  toJSON: { virtuals: true},
+  toObject: {virtuals: true}
+});
+
+
+var Room = mongoose.model('Room', schema);
+
+module.exports = Room;
--- routes/rooms.js
+++ routes/rooms.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
+var Room = require('../models/Room');
 var multer = require('multer');
 var upload = multer({dest: 'uploads/'});
 
@@ -26,15 +27,39 @@ router.get('/room', function(req, res, next) {
   res.render('rooms/index');
 });
 
+// Go Register Room Page
 router.get('/host',needAuth,function(req, res, next) {
   res.render('rooms/host');
 });
 
+// Register Room
 router.post('/host', upload.single('room_image'), function(req, res, next) {
   console.log(req.body);
   console.log(req.file);
 
-  res.render('rooms/step');
+  var newRoom = new Room({
+    host_email: req.body.host_email,
+    room_name: req.body.input_name,
+    room_intro: req.body.input_intro,
+    city: req.body.select_city,
+    post_code: req.body.post_code,
+    addr1: req.body.input_addr1,
+    addr2: req.body.input_addr2,
+    latitude: req.body.latitude,
+    longitude: req.body.longitude,
+    fee: req.body.input_fee,
+    facillities: req.body.input_facillities,
+    rules: req.body.input_rules,
+    image: req.file
+  });
+  newRoom.save(function(err) {
+    if (err) {
+      next(err);
+    } else {
+      req.flash('success', '숙소 등록이 완료되었습니다.');
+      res.render('rooms/step');
+    }
+  });
 });
 
 
