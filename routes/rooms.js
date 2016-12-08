dfe211269d855d495d309de5775f3f685e96f162
lunaseven <june9309@gmail.com>
2016-12-08 11:10:46 +0900

    fileuploading

--- routes/rooms.js
+++ routes/rooms.js
@@ -4,84 +4,98 @@ var fs = require('fs');
 var path = require('path');
 var Room = require('../models/Room');
 var multer = require('multer');
-var upload = multer({dest: './public/images/uploads/'});
+var upload = multer({
+    dest: './public/images/uploads/'
+});
 
 
 
 function needAuth(req, res, next) {
-  if (req.isAuthenticated()) {
-    next();
-  } else {
-    req.flash('danger', '로그인이 필요합니다.');
-    res.redirect('/');
-  }
+    if (req.isAuthenticated()) {
+        next();
+    } else {
+        req.flash('danger', '로그인이 필요합니다.');
+        res.redirect('/');
+    }
 }
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  Room.find({}, function (err, rooms) {
-      if (err) {
-          next(err);
-      } else {
-          res.render('rooms/list', {rooms: rooms});
-      }
-  });
+    Room.find({}, function(err, rooms) {
+        if (err) {
+            next(err);
+        } else {
+            res.render('rooms/list', {
+                rooms: rooms
+            });
+        }
+    });
 });
 
 
 
 router.get('/room', function(req, res, next) {
-  res.render('rooms/index');
+var room_id = req.query.id;
+Room.findById(room_id, function(err, room) {
+    if (err) {
+        return next(err);
+    }
+    res.render('rooms/index', {
+        room: room
+    });
+});
+
+
 });
 router.get('/:id/img', function(req, res, next) {
-  Room.findById(req.params.id, function(err, room) {
-      if (err) next(err);
-      else {
-          res.set({
-              "Content-Disposition": 'attachment; filename="' + room.image.originalname + '"',
-              "Content-Type": room.image.mimetype
-          });
-          var st = fs.createReadStream(path.join(__dirname, "../" + room.image.path));
-          st.on('error', function () {
-              res.status(404).end();
-          });
-          st.pipe(res);
-      }
-  });
-})
-// Go Register Room Page
-router.get('/host',needAuth,function(req, res, next) {
-  res.render('rooms/host');
+        Room.findById(req.params.id, function(err, room) {
+            if (err) next(err);
+            else {
+                res.set({
+                    "Content-Disposition": 'attachment; filename="' + room.image.originalname + '"',
+                    "Content-Type": room.image.mimetype
+                });
+                var st = fs.createReadStream(path.join(__dirname, "../" + room.image.path));
+                st.on('error', function() {
+                    res.status(404).end();
+                });
+                st.pipe(res);
+            }
+        });
+    })
+    // Go Register Room Page
+router.get('/host', needAuth, function(req, res, next) {
+    res.render('rooms/host');
 });
 
 // Register Room
 router.post('/host', upload.single('room_image'), function(req, res, next) {
-  console.log(req.body);
-  console.log(req.file);
+    console.log(req.body);
+    console.log(req.file);
 
-  var newRoom = new Room({
-    host_email: req.body.host_email,
-    room_name: req.body.input_name,
-    room_intro: req.body.input_intro,
-    city: req.body.select_city,
-    post_code: req.body.post_code,
-    addr1: req.body.input_addr1,
-    addr2: req.body.input_addr2,
-    latitude: req.body.latitude,
-    longitude: req.body.longitude,
-    fee: req.body.input_fee,
-    facillities: req.body.input_facillities,
-    rules: req.body.input_rules,
-    image: req.file
-  });
-  newRoom.save(function(err) {
-    if (err) {
-      next(err);
-    } else {
-      req.flash('success', '숙소 등록이 완료되었습니다.');
-      res.render('rooms/step');
-    }
-  });
+    var newRoom = new Room({
+        host_email: req.body.host_email,
+        room_name: req.body.input_name,
+        room_intro: req.body.input_intro,
+        city: req.body.select_city,
+        post_code: req.body.post_code,
+        addr1: req.body.input_addr1,
+        addr2: req.body.input_addr2,
+        latitude: req.body.latitude,
+        longitude: req.body.longitude,
+        fee: req.body.input_fee,
+        facillities: req.body.input_facillities,
+        rules: req.body.input_rules,
+        image: req.file
+    });
+    newRoom.save(function(err) {
+        if (err) {
+            next(err);
+        } else {
+            req.flash('success', '숙소 등록이 완료되었습니다.');
+            res.render('rooms/step');
+        }
+    });
 });
 
 
